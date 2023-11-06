9304c4a36042047c0366a55ff665873de46b347b
Victor Francis <cisfran05@protonmail.com>
2023-11-06 14:28:25 +0100

    Update hello.bat

--- hello.bat
+++ hello.bat
@@ -1,3 +1,18 @@
-echo msgbox "Please Wait While we install necessary packages for You!.Window will be closed after Installation!!!" > %tmp%\tmp.vbs
-wscript %tmp%\tmp.vbs
-del %tmp%\tmp.vbs
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    (echo Set objShell = CreateObject("WScript.Shell"^)
+     echo objShell.Popup "NO ADMIN!!!", 0, "Admin Check", 16
+    ) > "%temp%\tmp.vbs"
+    wscript "%temp%\tmp.vbs"
+    del "%temp%\tmp.vbs"
+) else (
+    (echo Set objShell = CreateObject("WScript.Shell"^)
+     echo objShell.Popup "YES ADMIN!!!", 0, "Admin Check", 64
+    ) > "%temp%\tmp.vbs"
+    wscript "%temp%\tmp.vbs"
+    del "%temp%\tmp.vbs"
+)
+
+pause
